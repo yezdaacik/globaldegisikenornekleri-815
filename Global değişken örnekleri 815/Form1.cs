b192e806764c1b047524fcf3ecedfe3594e1b696
yezdanuracik <145451081+yezdanuracik@users.noreply.github.com>
2023-10-10 15:07:59 +0300

    Add files via upload

---
+++ Global değişken örnekleri 815/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Global_değişken_örnekleri_815
+{
+    
+    public partial class Form1 : Form
+    {
+        double bakiye = 200;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBakiye.Text="Bakiye :" + bakiye.ToString();
+            lblBakiye.ForeColor = Color.Green;
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            bakiye += 50;
+            lblBakiye.Text = "Bakiye :" + bakiye.ToString();
+            lblBakiye.ForeColor = Color.Green;
+            
+        }
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+           if (bakiye >= 8)
+            {
+                bakiye -= 8;
+                lblBakiye.Text = "Bakiye :" + bakiye.ToString();
+            }
+
+           else
+            {
+                lblBakiye.Text = "Yetersiz bakiye";
+                lblBakiye.ForeColor= Color.Red;
+            }
+           
+
+        }
+    }
+}
